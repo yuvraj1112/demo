1f93af4dee502cdbed4d3b84633877c1f21fbcb2
Yuvraj Deshmukh <yuvraj1112@gmail.com>
2022-05-01 20:47:27 +0530

    Modifying data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into role values ('10','Admin');
 insert into role values ('11','Super User');
 insert into user values ('102 ','P','Q',null);
-insert into user values ('103 ','E','T','10');
+insert into user values ('103 ','E','T','11');
